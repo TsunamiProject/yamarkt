b27df8e1a0005522413730f9f7481f39277de068
tsproj <p.tsunami@yandex.ru>
2022-12-01 02:37:29 +0300

    balance/withdrawal models added

--- internal/models/model.go
+++ internal/models/model.go
@@ -1,6 +1,28 @@
 package models
 
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
 type Credentials struct {
 	Login string `json:"login"`
 	Pass  string `json:"pass"`
+}
+
+type Withdrawal struct {
+	Order string `json:"order"`
+	Sum   int    `json:"sum"`
+}
+
+type WithdrawalList struct {
+	Order       string    `json:"order"`
+	Sum         int       `json:"sum"`
+	ProcessedAt time.Time `json:"processed_at"`
+}
+
+type CurrentBalance struct {
+	Current   decimal.Decimal `json:"current"`
+	Withdrawn int             `json:"withdrawn"`
 }
