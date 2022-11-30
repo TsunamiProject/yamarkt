aa5bfbed2c2f18ca8b72e7abd42459599edff662
tsproj <p.tsunami@yandex.ru>
2022-12-01 01:36:31 +0300

    postgres storage template with conn method added

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,13 +1,34 @@
 package storage
 
-type Storage interface {
-	RegisterUser()
-	AuthUser()
+import (
+	"context"
+	"database/sql"
+	"fmt"
 
-	GetUserBalance()
-	GetUserOrders()
-	GetUserWithdrawals()
+	"github.com/TsunamiProject/yamarkt/internal/config"
+)
 
-	CreateUserOrder()
-	CreateUserWithdraw()
+type PostgresStorage struct {
+	PostgresQL *sql.DB
+}
+
+func NewPostgresStorage(databaseDsn string) (*PostgresStorage, error) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, config.StorageContextTimeout)
+	defer cancel()
+
+	_, err := connectToPostgresKernel(databaseDsn)
+	if err != nil {
+		return nil, err
+	}
+	//...
+	return nil, nil
+}
+
+func connectToPostgresKernel(databaseDsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", databaseDsn)
+	if err != nil {
+		return nil, fmt.Errorf("connection error to Postgres kernel with creds: %s", databaseDsn)
+	}
+	return db, nil
 }
