9e53b12f82f5f0d7424a265f79de200e5edb5921
tsproj <p.tsunami@yandex.ru>
2022-12-01 02:38:00 +0300

    balance storage template added

--- internal/storage/balance_storage.go
+++ internal/storage/balance_storage.go
@@ -1,1 +1,19 @@
 package storage
+
+import (
+	"context"
+
+	"github.com/TsunamiProject/yamarkt/internal/models"
+)
+
+func (ps *PostgresStorage) CreateWithdrawal(ctx context.Context, login string, withdrawal models.Withdrawal) error {
+	return nil
+}
+
+func (ps *PostgresStorage) GetWithdrawalList(ctx context.Context, login string) (wl []models.WithdrawalList, err error) {
+	return nil, nil
+}
+
+func (ps *PostgresStorage) GetCurrentBalance(ctx context.Context, login string) (cb models.CurrentBalance, err error) {
+	return models.CurrentBalance{}, nil
+}
