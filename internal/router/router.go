803b8c13998370ac6860506c412422b8ad438056
tsproj <p.tsunami@yandex.ru>
2022-12-01 01:35:42 +0300

    user handler methods added to user router group

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,23 +6,22 @@ import (
 	"github.com/TsunamiProject/yamarkt/internal/handler"
 )
 
-func NewRouter(h *handler.RequestHandler) chi.Router {
+func NewRouter(uh *handler.UserHandler) chi.Router {
 	router := chi.NewRouter()
 
 	router.Group(func(r chi.Router) {
 		router.Use() //middleware
 		router.Post("/api/user/orders", nil)
-		router.Post("/apu/user/balance/withdraw", nil)
+		router.Post("/apu/user/balance/withdrawals", nil)
 		router.Get("/api/user/orders", nil)
 		router.Get("/api/user/balance", nil)
 		router.Get("/api/user/withdrawals", nil)
 	})
 
 	router.Group(func(r chi.Router) {
-		router.Post("/api/user/register", nil)
-		router.Post("/api/user/login", nil)
+		router.Post("/api/user/register", uh.Register)
+		router.Post("/api/user/login", uh.Auth)
 	})
 
 	return router
-
 }
