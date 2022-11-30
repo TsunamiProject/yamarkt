353f57f2a95551491ecbac71677021abdb0279e0
tsproj <p.tsunami@yandex.ru>
2022-12-01 01:33:29 +0300

    user handler template added

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,4 +1,29 @@
 package handler
 
-type RequestHandler struct {
+import (
+	"context"
+	"net/http"
+
+	"github.com/TsunamiProject/yamarkt/internal/models"
+)
+
+type UserServiceProvider interface {
+	Auth(ctx context.Context, cred models.Credentials) error
+	Register(ctx context.Context, cred models.Credentials) error
+}
+
+type UserHandler struct {
+	service UserServiceProvider
+}
+
+func NewUserHandler(userHandler UserServiceProvider) *UserHandler {
+	return &UserHandler{service: userHandler}
+}
+
+func (uh UserHandler) Auth(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (uh UserHandler) Register(w http.ResponseWriter, r *http.Request) {
+
 }
