09b232b5e84b499496bf42b7139ce0797d3771e7
tsproj <p.tsunami@yandex.ru>
2022-12-01 05:20:33 +0300

    order storage template added

--- internal/storage/order_storage.go
+++ internal/storage/order_storage.go
@@ -1,1 +1,15 @@
 package storage
+
+import (
+	"context"
+
+	"github.com/TsunamiProject/yamarkt/internal/models"
+)
+
+func (ps *PostgresStorage) CreateOrder(ctx context.Context, login string, orderID string) error {
+	return nil
+}
+
+func (ps *PostgresStorage) OrderList(ctx context.Context, login string) (ol []models.OrderList, err error) {
+	return nil, nil
+}
