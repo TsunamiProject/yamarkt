3a09d1e185a541dd97c46827501142bd00c87281
tsproj <p.tsunami@yandex.ru>
2022-12-02 03:05:50 +0300

    user service methods added

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
 
 	"github.com/TsunamiProject/yamarkt/internal/models"
 )
@@ -20,9 +23,29 @@ func NewUserService(userStorage UserStorage) *UserService {
 }
 
 func (us *UserService) Register(ctx context.Context, cred models.Credentials) error {
+	encodedPass, err := Encode(cred.Pass)
+	if err != nil {
+		log.Printf("encoding password error: %s", err)
+		return err
+	}
+	err = us.userStorage.Register(ctx, cred.Login, encodedPass)
 	return nil
 }
 
 func (us *UserService) Auth(ctx context.Context, cred models.Credentials) error {
+	encodedPass, err := Encode(cred.Pass)
+	if err != nil {
+		log.Printf("encoding password error: %s", err)
+		return err
+	}
+	err = us.userStorage.Auth(ctx, cred.Login, encodedPass)
 	return nil
+}
+
+func Encode(src string) (encodedString string, err error) {
+	crInst := sha256.New()
+	crInst.Write([]byte(src))
+	srcBytes := crInst.Sum(nil)
+	encodedString = hex.EncodeToString(srcBytes)
+	return encodedString, nil
 }
