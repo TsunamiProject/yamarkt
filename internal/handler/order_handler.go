b8d5fb5f10e07d6514ee8ebf0027c3ff34c2a179
tsproj <p.tsunami@yandex.ru>
2022-12-04 04:02:27 +0300

    order handler methods added

--- internal/handler/order_handler.go
+++ internal/handler/order_handler.go
@@ -2,8 +2,18 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/ShiraazMoollatjie/goluhn"
+
+	"github.com/TsunamiProject/yamarkt/internal/config"
+	customErr "github.com/TsunamiProject/yamarkt/internal/customerrs"
 	"github.com/TsunamiProject/yamarkt/internal/models"
 )
 
@@ -21,9 +31,64 @@ func NewOrderHandler(osp OrderServiceProvider) *OrderHandler {
 }
 
 func (oh OrderHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error while reading body: %s", err)
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
 
+	stringBody := fmt.Sprintf("%s", body)
+	_, err = strconv.ParseInt(stringBody, 10, 0)
+	if err != nil {
+		log.Printf("Error while converting request body to int: %s", err)
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+	}
+
+	err = goluhn.Validate(stringBody)
+	if err != nil {
+		log.Printf("Error while validating request body vai Luhn algo: %s", err)
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+	}
+
+	ctx, cancel := context.WithTimeout(r.Context(), config.StorageContextTimeout)
+	defer cancel()
+
+	//TODO: get login from request
+	login := ""
+
+	err = oh.service.CreateOrder(ctx, login, stringBody)
+	if errors.As(err, &customErr.ErrOrderAlreadyExists) {
+		w.WriteHeader(http.StatusOK)
+	} else if errors.As(err, &customErr.ErrOrderCreatedByAnotherLogin) {
+		w.WriteHeader(http.StatusConflict)
+	} else if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func (oh OrderHandler) OrderList(w http.ResponseWriter, r *http.Request) {
-	
+	//TODO: get login from request
+	login := ""
+	ctx, cancel := context.WithTimeout(r.Context(), config.StorageContextTimeout)
+	defer cancel()
+
+	orderList, err := oh.service.OrderList(ctx, login)
+	if errors.As(err, &customErr.ErrNoOrders) {
+		log.Printf("no orders for %s login", login)
+		w.WriteHeader(http.StatusNoContent)
+	} else if errors.As(err, &customErr.ErrUnauthorizedUser) {
+		log.Printf("unauthorized login: %s", login)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else if err != nil {
+		log.Printf(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(orderList)
+	if err != nil {
+		log.Printf(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
 }
