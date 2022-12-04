ccbfc9544abd8396258bcc0965dc2020207518c0
tsproj <p.tsunami@yandex.ru>
2022-12-04 04:01:57 +0300

    new errors added

--- internal/customerrs/errors.go
+++ internal/customerrs/errors.go
@@ -3,6 +3,11 @@ package customerrs
 import "errors"
 
 var (
-	ErrUserDoesNotExist  = errors.New("user doesn't exist")
-	ErrUserAlreadyExists = errors.New("user already exists")
+	ErrUserDoesNotExist           = errors.New("user doesn't exist")
+	ErrUserAlreadyExists          = errors.New("user already exists")
+	ErrOrderAlreadyExists         = errors.New("order already exists")
+	ErrOrderCreatedByAnotherLogin = errors.New("order already created by another customer")
+	ErrUnauthorizedUser           = errors.New("unauthorized user")
+	ErrNoOrders                   = errors.New("no orders created by login")
+	ErrNoFunds                    = errors.New("no funds on login balance")
 )
