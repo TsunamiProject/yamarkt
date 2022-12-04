4f7d9d40e1d0881197ecb0fc3ddcd2df5b8945d0
tsproj <p.tsunami@yandex.ru>
2022-12-04 04:03:16 +0300

    order service methods added

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/TsunamiProject/yamarkt/internal/models"
 )
@@ -24,9 +29,35 @@ func NewOrderService(os OrderStorage, accURL string) *OrderService {
 }
 
 func (os *OrderService) CreateOrder(ctx context.Context, login string, orderID string) error {
+	err := os.storage.CreateOrder(ctx, login, orderID)
+	if err != nil {
+		return err
+	}
+	accOrder := models.AccrualJSON{Order: orderID}
+	accOrderJSON, err := json.Marshal(accOrder)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", os.AccrualURL+"/api/orders", bytes.NewBuffer(accOrderJSON))
+	if err != nil {
+		return err
+	}
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("error while making request to accrual service: %s", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	//TODO: worker for updating order info?
+
 	return nil
 }
 
 func (os *OrderService) OrderList(ctx context.Context, login string) (ol []models.OrderList, err error) {
-	return nil, nil
+	orderList, err := os.storage.OrderList(ctx, login)
+	return orderList, err
 }
