38cca0d83590529e99d9cec7959272a1bcc295e5
tsproj <p.tsunami@yandex.ru>
2022-12-06 03:24:16 +0300

    storage consturctor and close connection method added

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 
 	"github.com/TsunamiProject/yamarkt/internal/config"
 )
@@ -17,18 +20,50 @@ func NewPostgresStorage(databaseDsn string) (*PostgresStorage, error) {
 	ctx, cancel := context.WithTimeout(ctx, config.StorageContextTimeout)
 	defer cancel()
 
-	_, err := connectToPostgresKernel(databaseDsn)
+	pdb, err := connectToPostgresKernel(databaseDsn)
 	if err != nil {
+		log.Println("??")
 		return nil, err
 	}
-	//...
-	return nil, nil
+
+	_, err = pdb.Exec(usersTableQuery)
+	if err != nil {
+		log.Printf("error while creating users table: %s", err)
+		return nil, err
+	}
+
+	_, err = pdb.Exec(balanceTableQuery)
+	if err != nil {
+		log.Printf("error while creating balance table: %s", err)
+		return nil, err
+	}
+
+	_, err = pdb.Exec(ordersTableQuery)
+	if err != nil {
+		log.Printf("error while creating orders table: %s", err)
+		return nil, err
+	}
+
+	_, err = pdb.Exec(withdrawalsTableQuery)
+	if err != nil {
+		log.Printf("error while creating withdrawals table: %s", err)
+		return nil, err
+	}
+	return &PostgresStorage{PostgresQL: pdb}, nil
 }
 
 func connectToPostgresKernel(databaseDsn string) (*sql.DB, error) {
 	db, err := sql.Open("pgx", databaseDsn)
 	if err != nil {
-		return nil, fmt.Errorf("connection error to Postgres kernel with creds: %s", databaseDsn)
+		return nil, fmt.Errorf("connection error to Postgres kernel with creds: %s. %s", databaseDsn, err)
 	}
 	return db, nil
+}
+
+func (ps *PostgresStorage) CloseConnection() error {
+	err := ps.PostgresQL.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
