8c1aa49234c2b91da9836bcc526237e08b8b37c6
tsproj <p.tsunami@yandex.ru>
2022-12-06 03:22:00 +0300

    gzip middleware added

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,13 +4,14 @@ import (
 	"github.com/go-chi/chi/v5"
 
 	"github.com/TsunamiProject/yamarkt/internal/handler"
+	"github.com/TsunamiProject/yamarkt/internal/middleware"
 )
 
 func NewRouter(uh *handler.UserHandler, bh *handler.BalanceHandler, oh *handler.OrderHandler) chi.Router {
 	router := chi.NewRouter()
+	router.Use(middleware.GzipRespWriter, middleware.GzipReqReader)
 
 	router.Group(func(r chi.Router) {
-		router.Use() //middleware
 		router.Post("/api/user/orders", oh.CreateOrder)
 		router.Post("/apu/user/balance/withdrawals", bh.NewWithdrawal)
 		router.Get("/api/user/orders", oh.OrderList)
