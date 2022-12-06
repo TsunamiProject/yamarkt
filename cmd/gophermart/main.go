5b71a18e51481039f091797be6086d4992bb8c56
tsproj <p.tsunami@yandex.ru>
2022-12-06 03:00:25 +0300

    server start with params

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/TsunamiProject/yamarkt/internal/config"
 	"github.com/TsunamiProject/yamarkt/internal/handler"
+	appRouter "github.com/TsunamiProject/yamarkt/internal/router"
 	"github.com/TsunamiProject/yamarkt/internal/service"
 	"github.com/TsunamiProject/yamarkt/internal/storage"
 )
@@ -15,14 +17,26 @@ func main() {
 	cfg := config.New()
 	log.Print(cfg)
 
-	pStorage, _ := storage.NewPostgresStorage(cfg.DatabaseDSN)
+	pStorage, err := storage.NewPostgresStorage(cfg.DatabaseDSN)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	userService := service.NewUserService(pStorage)
 	userHandler := handler.NewUserHandler(userService)
 	log.Println(userHandler)
+
 	balanceService := service.NewBalanceService(pStorage)
 	balanceHandler := handler.NewBalanceHandler(balanceService)
 	log.Println(balanceHandler)
+
 	orderService := service.NewOrderService(pStorage, cfg.AccrualURL)
 	orderHandler := handler.NewOrderHandler(orderService)
 	log.Println(orderHandler)
+
+	router := appRouter.NewRouter(userHandler, balanceHandler, orderHandler)
+
+	httpServer := &http.Server{Addr: cfg.ServerAddress, Handler: router}
+	log.Fatal(httpServer.ListenAndServe())
+
 }
