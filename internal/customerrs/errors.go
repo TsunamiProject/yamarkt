ef61e5028aab9b7382ce0b5673fd065cd404bbab
tsproj <p.tsunami@yandex.ru>
2022-12-07 00:48:03 +0300

    new errors added

--- internal/customerrs/errors.go
+++ internal/customerrs/errors.go
@@ -3,12 +3,14 @@ package customerrs
 import "errors"
 
 var (
-	ErrUserDoesNotExist           = errors.New("user doesn't exist")
-	ErrUserAlreadyExists          = errors.New("user already exists")
-	ErrOrderAlreadyExists         = errors.New("order already exists")
-	ErrOrderCreatedByAnotherLogin = errors.New("order already created by another customer")
-	ErrUnauthorizedUser           = errors.New("unauthorized user")
-	ErrNoOrders                   = errors.New("no orders created by login")
-	ErrNoFunds                    = errors.New("no funds on login balance")
-	ErrWrongPassword              = errors.New("wrong password received")
+	ErrUserDoesNotExist            = errors.New("user doesn't exist")
+	ErrUserAlreadyExists           = errors.New("user already exists")
+	ErrOrderAlreadyExists          = errors.New("order already exists")
+	ErrOrderCreatedByAnotherLogin  = errors.New("order already created by another customer")
+	ErrWithdrawalOrderAlreadyExist = errors.New("withdrawal order already exists")
+	ErrUnauthorizedUser            = errors.New("unauthorized user")
+	ErrNoOrders                    = errors.New("no orders created by login")
+	ErrNoFunds                     = errors.New("no funds on login balance")
+	ErrNoWithdrawals               = errors.New("no withdrawals by login")
+	ErrWrongPassword               = errors.New("wrong password received")
 )
