92e3c3702223099e8d39a9a2274ca60e79d3917c
tsproj <p.tsunami@yandex.ru>
2022-12-08 07:46:48 +0300

    new middlewares added

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth/v5"
 
 	"github.com/TsunamiProject/yamarkt/internal/config"
@@ -11,13 +12,15 @@ import (
 
 func NewRouter(uh *handler.UserHandler, bh *handler.BalanceHandler, oh *handler.OrderHandler) chi.Router {
 	router := chi.NewRouter()
+	router.Use(chiMiddleware.Recoverer)
+	router.Use(chiMiddleware.Logger)
 	router.Use(middleware.GzipRespWriter, middleware.GzipReqReader)
 
 	router.Group(func(r chi.Router) {
 		r.Use(jwtauth.Verifier(config.TokenAuth))
 		r.Use(jwtauth.Authenticator)
 		router.Post("/api/user/orders", oh.CreateOrder)
-		router.Post("/apu/user/balance/withdrawals", bh.CreateWithdrawal)
+		router.Post("/api/user/balance/withdraw", bh.CreateWithdrawal)
 		router.Get("/api/user/orders", oh.OrderList)
 		router.Get("/api/user/balance", bh.GetCurrentBalance)
 		router.Get("/api/user/withdrawals", bh.GetWithdrawalList)
