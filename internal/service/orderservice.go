b950db5cc3b4e9274dae4d4dc42654e8ffbf12b1
tsproj <p.tsunami@yandex.ru>
2022-12-11 06:38:27 +0300

    unused comments removed

--- internal/service/orderservice.go
+++ internal/service/orderservice.go
@@ -3,8 +3,6 @@ package service
 import (
 	"context"
 
-	"github.com/rs/zerolog/log"
-
 	"github.com/TsunamiProject/yamarkt/internal/models"
 )
 
@@ -27,31 +25,6 @@ func NewOrderService(os OrderStorage, accURL string) *OrderService {
 func (os *OrderService) CreateOrder(ctx context.Context, login string, orderID string) (err error) {
 	//calling CreateOrder postgres storage method for creating new order
 	err = os.storage.CreateOrder(ctx, login, orderID)
-	if err != nil {
-		log.Printf("CreateOrder service. Create order storage error: %s", err)
-		return err
-	}
-	//accOrder := models.AccrualJSON{Order: orderID}
-	//accOrderJSON, err := json.Marshal(accOrder)
-	//if err != nil {
-	//	log.Printf("error while marshalling json for accrual service: %s", err)
-	//	return err
-	//}
-	//req, err := http.NewRequest("POST", os.AccrualURL+"/api/orders", bytes.NewBuffer(accOrderJSON))
-	//if err != nil {
-	//	return err
-	//}
-	//client := http.Client{
-	//	Timeout: 5 * time.Second,
-	//}
-	//resp, err := client.Do(req)
-	//if err != nil {
-	//	log.Printf("error while making request to accrual service: %s", err)
-	//	return err
-	//}
-	//defer resp.Body.Close()
-	//log.Printf("%d", resp.StatusCode)
-
 	return err
 }
 
